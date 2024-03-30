3522b4977c805ed97173f2c22e7106a869463e5a
Tom Handcock <33254448+TomRHandcock@users.noreply.github.com>
2024-03-30 22:45:23 +0000

    chore: Register `DurationSetterCubit` in GetIt

--- lib/src/engine/service_locator/root_setup.dart
+++ lib/src/engine/service_locator/root_setup.dart
@@ -3,6 +3,8 @@ import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:time_keeper/config/objectbox/objectbox.g.dart';
 import 'package:time_keeper/src/engine/feature/tracking_preference/tracking_preference_repository.dart';
+import 'package:time_keeper/src/engine/feature/tracking_preference/tracking_preference_store.dart';
+import 'package:time_keeper/src/ui/screens/duration_setter/cubit/duration_setter_cubit.dart';
 
 part 'bloc_setup.dart';
 part 'use_case_setup.dart';
