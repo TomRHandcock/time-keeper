e39803c703a9912d5ad3ba7215187617e328573a
Tom Handcock <33254448+TomRHandcock@users.noreply.github.com>
2024-04-01 14:47:48 +0100

    feat: Implement `TrackingPreferenceCache`

---
+++ lib/src/engine/feature/tracking_preference/tracking_preference_cache.dart
@@ -0,0 +1,4 @@
+import 'package:time_keeper/src/engine/base/simple_cache.dart';
+import 'package:time_keeper/src/engine/feature/tracking_preference/models.dart';
+
+typedef TrackingPreferenceCache = SimpleMemoryCache<TrackingPreference>;
