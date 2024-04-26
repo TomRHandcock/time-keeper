19f9e76a6ded5cf68387ea2ffcf6a17e3ed24576
Tom Handcock <33254448+TomRHandcock@users.noreply.github.com>
2024-04-26 23:41:07 +0100

    feat: Implement `takeIf` + `takeUnless` extension methods on any type

--- lib/src/engine/utils/transform_utils.dart
+++ lib/src/engine/utils/transform_utils.dart
@@ -2,4 +2,8 @@ extension TransformUtils<T> on T {
   S let<S>(S Function(T it) transform) => transform(this);
 
   S? asOrNull<S>() => this is S ? this as S : null;
+
+  T? takeIf(bool Function(T) predicate) => predicate(this) ? this : null;
+
+  T? takeUnless(bool Function(T) predicate) => predicate(this) ? null : this;
 }
